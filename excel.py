840e47c5791f02cdd5f80f9ccbe35e23857ccebc
Joe Beltrami <jpbeltrami@gmail.com>
2017-01-07 17:15:36 -0800

    "Add function parameter for tojson"

--- excel.py
+++ excel.py
@@ -7,6 +7,8 @@ import simplejson as json
 import psycopg2
 import urllib.parse
 
+# 
+
 # set up the connection. Extract the database info from the URI and connect with psycopg2
 def connect():
     try:
@@ -27,22 +29,26 @@ def connect():
         print("<couldn't connect>")
 
 # opens the excel file, extracts data row by row, returns it in JSON
-def tojson():
-    # Open the workbook.
-    wb = xlrd.open_workbook('practice.xlsx')
-    # select the first sheet.
-    sheet1 = wb.sheet_by_index(0)
-    # open an empty list where to put the excel data.
-    excel_list = []
-    # go through each row of excel file adding a dictionry for each
-    for rownum in range(sheet1.nrows):
-        row_values = sheet1.row_values(rownum)
-        products = OrderedDict()
-        products['name'] = row_values[0]
-        products['age'] = row_values[1]
+def tojson(workbook):
+    # Check the file extension is correct
+    if workbook.endswith('.xls', '.xlsx'):
+        # Open the workbook.
+        wb = xlrd.open_workbook(workbook)
+        # select the first sheet.
+        sheet1 = wb.sheet_by_index(0)
+        # open an empty list where to put the excel data.
+        excel_list = []
+        # go through each row of excel file adding a dictionry for each
+        for rownum in range(sheet1.nrows):
+            row_values = sheet1.row_values(rownum)
+            products = OrderedDict()
+            products['name'] = row_values[0]
+            products['age'] = row_values[1]
 
-        excel_list.append(products)
-    return excel_list
+            excel_list.append(products)
+        return excel_list
+    else:
+        print('The file extension is not supported or is in uppercase.')
 
 # uses psycopg2 to create a query to update the DB.
 def post():
