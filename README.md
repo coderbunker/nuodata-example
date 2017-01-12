aa2845f7830e26018912b52b72f9981c0bcb5419
Joe Beltrami <jpbeltrami@gmail.com>
2017-01-12 11:49:01 +0800

    included argparse for user input

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # nuodata-example
 Example for Nuodata.io
 
-- tojson method found on: http://www.anthonydebarros.com/2013/02/05/get-json-from-excel-using-python-xlrd/
+###
 
-- url parse in connect from:
-http://stackoverflow.com/questions/15634092/connect-to-an-uri-in-postgres/32589487
+### References
+- tojson method found on: http://www.anthonydebarros.com/2013/02/05/get-json-from-excel-using-python-xlrd/
 
 - error handling seen on: http://stackoverflow.com/questions/8497886/graceful-primary-key-error-handling-in-python-psycopg2
--- excel.py
+++ excel.py
@@ -2,22 +2,35 @@
 # Reads an excel file and transforms to JSON.
 
 import xlrd
+import sys
 from collections import OrderedDict
 import simplejson as json
 import psycopg2
-import urllib.parse
+import argparse
 
-#
+# TODO update readme with what this does, and usage example.
+# empty comment on line 10
+# TODO fix custom exception line 29
+# useless undesirable check
+# TODO make parameter for database function and for post function.
 
 # set up the connection. Extract the database info from the URI and connect with psycopg2
+def takesArguments():
+    # parses the args containing the excel file directory and stores it into a variable.
+    parser = argparse.ArgumentParser(description='Sends information from an excel sheet to the nuodata DB.')
+    parser.add_argument('excel-file', help='the directory of the excel file you want to send to the db.')
+    parser.add_argument('database uri', help='this includes the uri of the db. include all required parameters in the uri.')
+
+    excel = parser.parse_args()
+    # Asks user for database api information.
+    database = input('database name please: ')
+    username = input('username please: ')
+    password = input('password please: ')
+    hostname = input('hostname please: ')
+
 def connect():
-    # enter your database API information.. either with the uri or the credentials
+    # connect with these credentials.
     try:
-        result = urllib.parse.urlparse("postgres://USERNAME:PASSWORD@db.nuodata.io:5432/4cebab44")
-        username = result.username
-        password = result.password
-        database = result.path[1:]
-        hostname = result.hostname
         DB = psycopg2.connect(
             database = database,
             user = username,
@@ -26,30 +39,26 @@ def connect():
         )
         c = DB.cursor()
         return DB, c
-    except psycopg2.Error as e:
-        print(e.pgerror)
+    except:
+        return('Could not connect plese check that your credentials have been entered correctly')
 
 # opens the excel file, extracts data row by row, returns it in JSON
 def tojson(workbook):
-    # Check the file extension is correct
-    if workbook.endswith('.xlsx'):
-        # Open the workbook.
-        wb = xlrd.open_workbook(workbook)
-        # select the first sheet.
-        sheet1 = wb.sheet_by_index(0)
-        # open an empty list where to put the excel data.
-        excel_list = []
-        # go through each row of excel file adding a dictionry for each
-        for rownum in range(sheet1.nrows):
-            row_values = sheet1.row_values(rownum)
-            products = OrderedDict()
-            products['name'] = row_values[0]
-            products['age'] = row_values[1]
+    # Open the workbook.
+    wb = xlrd.open_workbook(workbook)
+    # select the first sheet.
+    sheet1 = wb.sheet_by_index(0)
+    # open an empty list where to put the excel data.
+    excel_list = []
+    # go through each row of excel file adding a dictionry for each
+    for rownum in range(sheet1.nrows):
+        row_values = sheet1.row_values(rownum)
+        products = OrderedDict()
+        products['name'] = row_values[0]
+        products['age'] = row_values[1]
 
-            excel_list.append(products)
-        return excel_list
-    else:
-        print('The file extension is not supported or is in uppercase.')
+        excel_list.append(products)
+    return excel_list
 
 # uses psycopg2 to create a query to update the DB.
 def post():
@@ -65,6 +74,5 @@ def post():
         DB.commit()
     c.close()
     DB.close()
-
 
 post()
